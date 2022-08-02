34ecea7e300fbfc3784d1261b17d371f493e66a2
tmusinov <102414199+tmusinov@users.noreply.github.com>
2022-08-03 01:01:44 +0300

    Post functionality

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,17 +1,36 @@
 const router = require('express').Router();
 const postService = require('../services/postService');
 
-const isAuth = require('../middlewares/isAuthenticated');
 const { auth } = require('../middlewares/auth');
-const isGuest = require('../middlewares/isGuest');
+
 
 router.post('/create', auth, async (req, res, next) => {
     try {
         let result = await postService.create(req.body);
+        console.log(result);
         res.status(200).json({ result });
     } catch (error) {
-        console.log(error);
-        return json(error);
+        return res.status(400).json(error);
+    }
+});
+
+router.get('/all', auth, async (req, res, next) => {
+    try {
+        let result = await postService.getAll(req.body);
+        console.log(result);
+        res.status(200).json({ result });
+    } catch (error) {
+        return res.status(400).json(error);
+    }
+});
+
+router.post('/:_id/like', auth, async (req, res, next) => {
+    try {
+        let result = await postService.likePost(req.body);
+        console.log(result);
+        res.status(200).json({ result });
+    } catch (error) {
+        return res.status(400).json(error);
     }
 });
 
